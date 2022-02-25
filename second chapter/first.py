16fa671b8cb10b728a7e7954b5df8e3f3c53397c
Hazem Alkhamisi <65124870+H5misi@users.noreply.github.com>
2022-02-26 01:31:42 +0200

    Add files via upload

---
+++ second chapter/first.py
@@ -0,0 +1,24 @@
+
+
+print()
+
+
+
+
+# Selection sort
+lis = [5, 6, 3, 10, 1, 9, 8, 7, 2, 4]
+
+def selectionSort(lis):
+    minIndex = 0
+    for i in range(1, len(lis)):
+        if lis[i] < lis[minIndex]:
+            minIndex = i
+    
+    return minIndex
+
+newLis = []
+for i in range(len(lis)):
+    min = selectionSort(lis)
+    newLis.append(lis.pop(min))
+
+print(newLis)

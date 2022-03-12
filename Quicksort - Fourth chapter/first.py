71b1ead2a2795837ecc8fd8a4fb9c647a2be6434
Hazem Alkhamisi <65124870+H5misi@users.noreply.github.com>
2022-03-13 00:49:37 +0200

    Create first.py

---
+++ Quicksort - Fourth chapter/first.py
@@ -0,0 +1,24 @@
+
+
+print()
+
+
+
+# Quick sort
+
+lis = [10, 1, 5, 7, 2, 6, 4, 9, 3, 8]
+
+def quickSort(lis):
+    if len(lis) < 2:
+        return lis
+    else:
+        pivot = lis[0]
+        less = [i for i in lis[1:] if i <= pivot]
+        greater = [i for i in lis[1:] if i > pivot]
+        print(pivot, less, greater)
+        return quickSort(less) + [pivot] + quickSort(greater)
+        
+
+    
+
+print(quickSort(lis))
